bb1369da49f8b2e7d5e536602ac18d9ab9c61e2d
xcheche <chekwubechukwu23@gmail.com>
2025-05-14 10:48:39 +0100

    fixed
    fixed database

--- src/settings.py
+++ src/settings.py
@@ -125,19 +125,20 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
+        'OPTIONS': {
+            'sslmode': os.getenv('DATABASE_SSLMODE', 'require'),  # Default to 'require' if not set
+        },
+    }
+}
 
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
